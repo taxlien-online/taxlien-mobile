2274bb52e8f88d8eff86863235c8ed3b0dab89e5
Anton Dodonov <anton_dodonov@mail.ru>
2024-05-06 12:44:30 +0700

    commit

--- src/screens/VedicGamesScreen/index.js
+++ src/screens/VedicGamesScreen/index.js
@@ -10,7 +10,7 @@ var referer = "-";
 */
 export default function VedicGamesScreen() {
   const webViewRef= useRef()
-  const uri = 'https://quickex.io/';
+  const uri = 'https://eevee.market/';
   const [canGoBack, setCanGoBack] =useState(false);
   const [canGoForward, setCanGoForward] =useState(false);
   const handleBackPress =() => {
